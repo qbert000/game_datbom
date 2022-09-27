21ad1e741aea57d950511f63a3ffcb4437a96c32
qbert000 <quyen1412kid@gmail.com>
2022-09-28 00:11:30 +0700

    first commit

--- src/main/java/org/example/Factory.java
+++ src/main/java/org/example/Factory.java
@@ -1,7 +1,24 @@
 package org.example;
 
+import com.almasb.fxgl.dsl.components.ProjectileComponent;
+import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.EntityFactory;
+import com.almasb.fxgl.entity.SpawnData;
+import com.almasb.fxgl.entity.Spawns;
+import javafx.geometry.Point2D;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+import static com.almasb.fxgl.dsl.FXGL.entityBuilder;
 
 public class Factory implements EntityFactory {
 
+    @Spawns("enemy")
+    public Entity newEnemy(SpawnData data) {
+        return entityBuilder(data)
+                .view(new Rectangle(40, 40, Color.RED))
+                //.view("brick.png")
+                .with(new ProjectileComponent(new Point2D(1, 0), 10))
+                .build();
+    }
 }
